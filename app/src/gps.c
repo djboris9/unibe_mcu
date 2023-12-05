c60686f0c4e583e012580299c0c2f89d7f5a20c8
Boris Djurdjevic <boris@djurdjevic.ch>
2023-12-05 23:31:32 +0100

    gps reception works with dma

--- app/src/gps.c
+++ app/src/gps.c
@@ -6,8 +6,8 @@
 const struct device *const dev_usart = DEVICE_DT_GET(DT_NODELABEL(usart3));
 
 #define BUFLEN 128
-uint8_t buf_a[BUFLEN];
-uint8_t buf_b[BUFLEN];
+uint8_t buf[2][BUFLEN];
+int curbuf = 0;
 
 static void uart_callback(const struct device *dev, struct uart_event *evt, void *user_data) {
     switch (evt->type) {
@@ -19,14 +19,18 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
             break;
         case UART_RX_BUF_REQUEST:
             printf("UART RX buffer request\n");
-            // TODO: Rollover buffer
-            int ret = uart_rx_buf_rsp(dev, buf_b, BUFLEN);
+            // Rollover buffer
+            curbuf = (curbuf + 1) % 2;
+
+            // Respond to buffer request
+            int ret = uart_rx_buf_rsp(dev, buf[curbuf], BUFLEN);
             if (ret) {
                 printf("Cannot respond to buffer request: %d\n", ret);
             }
             break;
         case UART_RX_BUF_RELEASED:
             printf("UART RX buffer released\n");
+            printf("Received: %s\n", buf[curbuf]);
             break;
         case UART_RX_STOPPED:
             printf("UART RX stopped\n");
@@ -54,7 +58,7 @@ int gps_init(void) {
     uart_callback_set(dev_usart, uart_callback, NULL);
 
     // Enable RX
-    ret = uart_rx_enable(dev_usart, buf_a, BUFLEN, SYS_FOREVER_US);
+    ret = uart_rx_enable(dev_usart, buf[curbuf], BUFLEN, SYS_FOREVER_US);
     if (ret) {
         printk("Cannot enable RX: %d\n", ret);
         return -1;
